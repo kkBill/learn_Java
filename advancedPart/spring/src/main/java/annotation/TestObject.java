25a14361838a70b239028aaa7f68a485b50a84ac
kkBill <mjy332528@163.com>
2019-05-04 00:28:06 +0800

    添加注解方式注入

--- advancedPart/spring/src/main/java/annotation/TestObject.java
+++ advancedPart/spring/src/main/java/annotation/TestObject.java
@@ -1,29 +1,8 @@
 package annotation;
 
-import annotation.controller.UserController;
-import annotation.repository.UserRepository;
-import annotation.service.UserService;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class TestObject {
-    public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-annotation.xml");
-
-//        TestObject to = (TestObject) ctx.getBean("testObject");
-//        System.out.println(to);
-
-//        UserController userController = (UserController) ctx.getBean("userController");
-//        System.out.println(userController);
-
-        UserRepository userRepository = (UserRepository) ctx.getBean("userRepository");
-        System.out.println(userRepository);
-
-        UserService userService = (UserService) ctx.getBean("userService");
-        System.out.println(userService);
-
-    }
 
 }
